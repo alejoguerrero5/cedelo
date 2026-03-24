b6fe1689fd2ae4586a1f254829f6037a8f77505e
alejoguerrero5 <alejoguerrero5@gmail.com>
2026-03-23 20:54:45 -0500

    connect supabase with api

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,21 +1,25 @@
 // /app/api/contact/route.ts
 
+import { supabase } from "@/lib/supabase";
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
     const { name, email, phone, city, projectType, isVIS, terms } = body;
 
-    console.log("Nuevo lead:", {
-      name,
-      email,
-      phone,
-      city,
-      projectType,
-      isVIS,
-      terms,
-    });
+    const { error } = await supabase
+      .from("leads")
+      .insert([{ name, email, phone, city, projectType, isVIS, terms }]);
 
+    if (error) {
+      console.error(error);
+      return new Response(JSON.stringify({ error: "Error guardando lead" }), {
+        status: 500,
+      });
+    }
+
+    console.log("Lead guardado en DB");
     return new Response(JSON.stringify({ ok: true }), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ error: "Error en la API" }), {
